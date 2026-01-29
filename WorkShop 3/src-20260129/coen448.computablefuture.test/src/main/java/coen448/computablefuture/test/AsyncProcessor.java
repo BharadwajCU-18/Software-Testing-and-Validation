ca53cbb51e4f02151fe432f9600d148ccd560e0f
Bharadwaj Reddy Bangaru <bharadwajreddy.bangaru@mail.concordia.ca>
2026-01-29 17:13:32 -0500

    Add workshop3 async processor project with injected bugs

---
+++ WorkShop 3/src-20260129/coen448.computablefuture.test/src/main/java/coen448/computablefuture/test/AsyncProcessor.java
@@ -0,0 +1,21 @@
+package coen448.computablefuture.test;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class AsyncProcessor {
+    public CompletableFuture<String> processAsync(List<Microservice> microservices) {
+        if (microservices == null || microservices.isEmpty()) {
+            return CompletableFuture.completedFuture(null);
+        }
+        List<CompletableFuture<String>> futures = microservices.stream()
+            .map(client -> client.retrieveAsync("hello"))
+            .collect(Collectors.toList());
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+            .thenApply(v -> futures.stream()
+                .map(CompletableFuture::join)
+                .limit(1)
+                .collect(Collectors.joining(" ")));
+    }
+}
